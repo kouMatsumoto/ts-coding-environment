a52221a4784dab6e0fd80bda72692bbe8d9bf07d
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-06-04 15:05:42 +0900

    feat: update with process.stdin handling

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
+process.openStdin();
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+let input_string = '';
+
+process.stdin.on('data', (chunk: string) => {
+  input_string += chunk;
+});
+
+process.stdin.on('end', () => {
+  const lines = input_string.split('\n');
+  console.log('lines', lines);
+});
+
+
 export function myFunc() {
   return 'hello!';
 }
